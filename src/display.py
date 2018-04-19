86c59d69167e5d16f2ca71f35588b6d14d103561
abdulfatir <abdulfatirs@gmail.com>
2018-04-19 18:15:43 +0800

    Update plots

--- src/display.py
+++ src/display.py
@@ -1,6 +1,42 @@
 from __future__ import print_function
+import matplotlib.pyplot as plt
+import random
+random.seed(1337)
 
 
 def log_time(message, time, tabs=1):
     message = (tabs * '\t') + '[t] ' + message
     print('%s: %.4fs' % (message, time))
+
+
+def assign_markers(Y):
+    markers = ['o', 'v', '^', '<', '>', '8', 's', 'p', '*', 'h', 'H', 'D', 'd', 'P', 'X']
+    random.shuffle(markers)
+    marker_map = {}
+    M = []
+    idx = 0
+    for y in Y:
+        m = marker_map.get(y)
+        if m:
+            M.append(m)
+        else:
+            marker_map[y] = markers[idx]
+            idx += 1
+            M.append(marker_map[y])
+    return M
+
+
+def assign_colors(Y):
+    colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+    color_map = {}
+    C = []
+    idx = 0
+    for y in Y:
+        m = color_map.get(y)
+        if m:
+            C.append(m)
+        else:
+            color_map[y] = colors[idx]
+            idx += 1
+            C.append(color_map[y])
+    return C
--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from data_utils import normalize_dataset
 from subKmeans import sub_kmeans
 from matrix_utils import projection_matrix
 from sklearn.metrics import normalized_mutual_info_score as nmi
+from display import assign_markers, assign_colors
 
 
 X, Y = load_dataset('seeds')
@@ -17,8 +18,12 @@ for i in range(1):
         trans[None, :, :], np.transpose(X[:, None, :], [0, 2, 1]))
     X_rotated = X_rotated.squeeze(-1).T
     acc = nmi(Y, C)
+    M = assign_markers(C)
+    K = assign_colors(Y)
     print('[i] Results')
     print('[*] m: %d' % m)
     print('[*] NMI: %.5f' % acc)
-    plt.scatter(X_rotated[0], X_rotated[1], c=C)
+    data_points = zip(X_rotated[0], X_rotated[1], K, M)
+    for x_, y_, c_, m_ in data_points:
+        plt.scatter(x_, y_, c=c_, marker=m_)
     plt.show()
