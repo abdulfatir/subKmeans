50eb62483309670c0bf184feaa170ffe9c369c9e
Abdul Fatir Ansari <abdulfatirs@gmail.com>
2018-04-20 14:18:06 +0800

    Update kernel tests

--- src/custom_kernels.py
+++ src/custom_kernels.py
@@ -17,11 +17,9 @@ def init():
     mod = SourceModule(cuda_source)
 
 
-def matmul(a_gpu, b_gpu):
-    ss = time.time()
+def matmul(a_gpu, b_gpu, c_gpu):
     SHAPE_A = np.array(a_gpu.shape).astype(np.uint32)
     SHAPE_B = np.array(b_gpu.shape).astype(np.uint32)
-    c_gpu = gpuarray.empty((SHAPE_A[0], SHAPE_B[1]), np.float32)
     SHAPE_A_gpu = cuda.mem_alloc(SHAPE_A.nbytes)
     cuda.memcpy_htod(SHAPE_A_gpu, SHAPE_A)
     SHAPE_B_gpu = cuda.mem_alloc(SHAPE_B.nbytes)
@@ -31,13 +29,13 @@ def matmul(a_gpu, b_gpu):
     GRID_DIMX = int(np.ceil(SHAPE_B[1] / float(BLOCK_DIMX)))
     GRID_DIMY = int(np.ceil(SHAPE_A[0] / float(BLOCK_DIMY)))
     func = mod.get_function("matmul")
-    func(a_gpu, b_gpu, c_gpu.gpudata, SHAPE_A_gpu, SHAPE_B_gpu, block=(
+    func(a_gpu.gpudata, b_gpu.gpudata, c_gpu.gpudata, SHAPE_A_gpu, SHAPE_B_gpu, block=(
         BLOCK_DIMX, BLOCK_DIMY, 1), grid=(GRID_DIMX, GRID_DIMY, 1))
-    print(time.time() - ss)
-    return c_gpu
+    # return c_gpu
 
 
 def column_mean(a_gpu):
+    ss = time.time()
     SHAPE_A = np.array(a_gpu.shape).astype(np.uint32)
     SHAPE_A_gpu = cuda.mem_alloc(SHAPE_A.nbytes)
     cuda.memcpy_htod(SHAPE_A_gpu, SHAPE_A)
@@ -45,12 +43,14 @@ def column_mean(a_gpu):
     func = mod.get_function("column_mean")
     BLOCK_DIMX = 1024
     GRID_DIMX = int(np.ceil(a_gpu.shape[1] / float(BLOCK_DIMX)))
-    func(a_gpu, mean_a_gpu, SHAPE_A_gpu, block=(
+    func(a_gpu.gpudata, mean_a_gpu.gpudata, SHAPE_A_gpu, block=(
         BLOCK_DIMX, 1, 1), grid=(GRID_DIMX, 1, 1))
+    print('Func:', time.time() - ss)
     return mean_a_gpu
 
 
 def argmin_mu_diff(a_gpu, mu_gpu):
+    ss = time.time()
     SHAPE_A_gpu = gpuarray.to_gpu(np.asarray(
         [a_gpu.shape[0], a_gpu.shape[1]]).astype(np.uint32))
     SHAPE_MU_gpu = gpuarray.to_gpu(np.asarray(
@@ -59,12 +59,14 @@ def argmin_mu_diff(a_gpu, mu_gpu):
     func = mod.get_function("argmin_mu_diff")
     BLOCK_DIMX = 1024
     GRID_DIMX = int(np.ceil(a_gpu.shape[0] / float(BLOCK_DIMX)))
-    func(a_gpu, mu_gpu, SHAPE_A_gpu, SHAPE_MU_gpu, C_gpu,
+    func(a_gpu.gpudata, mu_gpu.gpudata, SHAPE_A_gpu, SHAPE_MU_gpu, C_gpu,
          block=(BLOCK_DIMX, 1, 1), grid=(GRID_DIMX, 1, 1))
+    print('Func:', time.time() - ss)
     return C_gpu
 
 
 def sum_axis2(a_gpu):
+    ss = time.time()
     BLOCK_DIMX = 32
     BLOCK_DIMY = 32
     GRID_DIMX = int(np.ceil(a_gpu.shape[2] / float(BLOCK_DIMX)))
@@ -74,19 +76,25 @@ def sum_axis2(a_gpu):
         [a_gpu.shape[0], a_gpu.shape[1], a_gpu.shape[2]]).astype(np.uint32))
     sum_gpu = gpuarray.empty(
         (a_gpu.shape[0], a_gpu.shape[2]), dtype=np.float32)
-    func(a_gpu, SHAPE_A_gpu, sum_gpu, block=(BLOCK_DIMX,
-                                             BLOCK_DIMY, 1), grid=(GRID_DIMX, GRID_DIMY, 1))
+    func(a_gpu.gpudata, SHAPE_A_gpu, sum_gpu.gpudata, block=(BLOCK_DIMX,
+                                                             BLOCK_DIMY, 1), grid=(GRID_DIMX, GRID_DIMY, 1))
+    print('Func:', time.time() - ss)
     return sum_gpu
+
+
+def _random_unif_ndarray(*shape):
+    return np.random.uniform(-2, 2, shape).astype(np.float32)
 
 
 def _test_matmul():
     print('[*] Testing matmul')
-    a = np.random.randn(10500, 19800).astype(np.float32)
-    b = np.random.randn(19800, 10500).astype(np.float32)
+    a = _random_unif_ndarray(125000, 1980)
+    b = _random_unif_ndarray(1980, 10500)
     a_gpu = gpuarray.to_gpu(a)
     b_gpu = gpuarray.to_gpu(b)
+    c_cuda = gpuarray.empty((a_gpu.shape[0], b_gpu.shape[1]), np.float32)
     t1 = time.time()
-    c_cuda = matmul(a_gpu, b_gpu)
+    matmul(a_gpu, b_gpu, c_cuda)
     t2 = time.time()
     c_np = np.matmul(a, b)
     t3 = time.time()
@@ -97,7 +105,7 @@ def _test_matmul():
 
 def _test_column_mean():
     print('[*] Testing column_mean')
-    a = np.random.randn(10500, 19800).astype(np.float32)
+    a = _random_unif_ndarray(10500, 19800)
     a_gpu = gpuarray.to_gpu(a)
     t1 = time.time()
     c_cuda = column_mean(a_gpu)
@@ -111,8 +119,8 @@ def _test_column_mean():
 
 def _test_argmin_mu_diff():
     print('[*] Testing argmin_mu_diff')
-    a = np.random.randn(19800, 10500).astype(np.float32)
-    b = np.random.randn(100, 10500).astype(np.float32)
+    a = _random_unif_ndarray(19800, 105)
+    b = _random_unif_ndarray(100, 105)
     a_gpu = gpuarray.to_gpu(a)
     b_gpu = gpuarray.to_gpu(b)
     t1 = time.time()
@@ -128,7 +136,7 @@ def _test_argmin_mu_diff():
 
 def _test_sum_axis2():
     print('[*] Testing sum_axis2')
-    a = np.random.randn(10500, 19800, 19800).astype(np.float32)
+    a = _random_unif_ndarray(10, 19800, 198)
     a_gpu = gpuarray.to_gpu(a)
     t1 = time.time()
     c_cuda = sum_axis2(a_gpu)
@@ -142,6 +150,8 @@ def _test_sum_axis2():
 
 if __name__ == '__main__':
     init()
+    start = cuda.Event()
+    end = cuda.Event()
     # Test matmul
     _test_matmul()
     # Test column_mean
